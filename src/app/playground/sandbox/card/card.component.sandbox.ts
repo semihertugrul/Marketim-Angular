85391213e847dabe9f6502b882672011d69eeb17
Semih Ertuğrul <semihertugrul@hotmail.com>
2019-06-13 13:55:36 +0300

    delete card sandbox

--- src/app/playground/sandbox/card/card.component.sandbox.ts
+++
@@ -1,7 +0,0 @@
-import { sandboxOf } from 'angular-playground';
-import { CardComponent } from '../card.component';
-
-export default sandboxOf(CardComponent)
-  .add('default', {
-    template: `<app-card></app-card>`
-  });
